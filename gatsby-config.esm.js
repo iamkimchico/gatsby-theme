80d65cf93690894c7dc54d24bedd0aa073d5f9bd
Kimchi-alive <kto@alivekirken.dk>
2021-05-31 14:54:37 +0200

    package test

--- gatsby-config.esm.js
+++ gatsby-config.esm.js
@@ -1,37 +1,37 @@
 // import { media } from "./src/constants/breakpoints"
 
-export default {
-  plugins: [
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-sass",
-    "gatsby-image",
-    {
-    resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "process.env.SITE_NAME",
-        short_name: "process.env.SITE_NAME",
-        start_url: `/`,
-        display: `standalone`,
-        icon: `src/images/gatsby-icon.png`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-breakpoints",
-      options: {
-        queries: "media",
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /assets/,
-        },
-      },
-    },
-  ],
-}
+// module.exports = {
+//   plugins: [
+//     "gatsby-transformer-sharp",
+//     "gatsby-plugin-sharp",
+//     "gatsby-plugin-sass",
+//     "gatsby-image",
+//     {
+//     resolve: `gatsby-plugin-manifest`,
+//       options: {
+//         name: "process.env.SITE_NAME",
+//         short_name: "process.env.SITE_NAME",
+//         start_url: `/`,
+//         display: `standalone`,
+//         icon: `src/images/gatsby-icon.png`,
+//         background_color: `#663399`,
+//         theme_color: `#663399`,
+//         display: `minimal-ui`,
+//       },
+//     },
+//     {
+//       resolve: "gatsby-plugin-breakpoints",
+//       options: {
+//         queries: "media",
+//       },
+//     },
+//     {
+//       resolve: "gatsby-plugin-react-svg",
+//       options: {
+//         rule: {
+//           include: /assets/,
+//         },
+//       },
+//     },
+//   ],  
+// }
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,44 @@
-require = require("esm")(module)
-module.exports = require("./gatsby-config.esm.js")
-
-// because I want to use ES6 syntax i have created this. Use gatsby-config.esm.js to do everything you would do in gatsby-config.
+module.exports = (props) => ({
+  plugins: [
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sass",
+    "gatsby-image",
+    {
+    resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: props.name,
+        short_name: props.name,
+        start_url: `/`,
+        display: `standalone`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-breakpoints",
+      options: {
+        queries: "media",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: props.prismicRepo,
+        accessToken: props.prismicToken,
+        prismicToolbar: true,
+        linkResolver: ({ node, key, value }) => (doc) => {
+          `/${doc.uid}`;
+        },
+        schemas: {},
+      },
+    },
+  ],  
+})
