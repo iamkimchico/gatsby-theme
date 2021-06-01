60e8b2e71221cd855a06a25baeda92a6d7cce702
Kimchi-alive <kto@alivekirken.dk>
2021-06-01 09:42:25 +0200

    update package

--- gatsby-config.esm.js
+++ gatsby-config.esm.js
@@ -1,37 +1,46 @@
-// import { media } from "./src/constants/breakpoints"
-
-// module.exports = {
-//   plugins: [
-//     "gatsby-transformer-sharp",
-//     "gatsby-plugin-sharp",
-//     "gatsby-plugin-sass",
-//     "gatsby-image",
-//     {
-//     resolve: `gatsby-plugin-manifest`,
-//       options: {
-//         name: "process.env.SITE_NAME",
-//         short_name: "process.env.SITE_NAME",
-//         start_url: `/`,
-//         display: `standalone`,
-//         icon: `src/images/gatsby-icon.png`,
-//         background_color: `#663399`,
-//         theme_color: `#663399`,
-//         display: `minimal-ui`,
-//       },
-//     },
-//     {
-//       resolve: "gatsby-plugin-breakpoints",
-//       options: {
-//         queries: "media",
-//       },
-//     },
-//     {
-//       resolve: "gatsby-plugin-react-svg",
-//       options: {
-//         rule: {
-//           include: /assets/,
-//         },
-//       },
-//     },
-//   ],  
-// }
+export default (props) => ({
+  plugins: [
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sass",
+    "gatsby-image",
+    {
+    resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: props.name,
+        short_name: props.name,
+        start_url: `/`,
+        display: `standalone`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-breakpoints",
+      options: {
+        queries: "media",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: props.prismicRepo,
+        accessToken: props.prismicToken,
+        prismicToolbar: true,
+        linkResolver: ({ node, key, value }) => (doc) => {
+          `/${doc.uid}`;
+        },
+        schemas: {
+          page:require("./prismic/schemas/page.json"),
+        },
+      },
+    },
+  ],  
+})
