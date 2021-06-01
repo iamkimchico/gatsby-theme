11976b4cd889e6286e3ecde8584c1f6c0f9e9ee9
Kimchi-alive <kto@alivekirken.dk>
2021-06-01 13:53:13 +0200

    update package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,22 @@
-module.exports = (props) => ({
-  plugins: [
+// const requireEsm = require('esm')(module);
+// module.exports = requireEsm('./gatsby-config.esm.js');
+
+module.exports = {
+  plugins:[
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
-    "gatsby-plugin-sass",
-    "gatsby-image",
     {
-    resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: props.name,
-        short_name: props.name,
-        start_url: `/`,
-        display: `standalone`,
-        icon: `src/images/gatsby-icon.png`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-breakpoints",
-      options: {
-        queries: "media",
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /assets/,
+      resolve: 'gatsby-source-prismic',
+      options:{
+        repositoryName:"gatsby-theme",
+        accessToken:"MC5ZTFRtZHhBQUFDWUFlcExw.77-9ce-_ve-_ve-_vSHvv73vv70U77-977-9PAXvv73vv73vv73vv71LNO-_ve-_vW9k77-977-977-977-977-977-977-977-977-9",
+        linkResolver: ({ node, key, value }) => (doc) => {
+          // do something with links here
         },
-      },
+        schemas:{
+          page: require("./prismic/schemas/page.json")
+        }
+      }
     }
-  ],  
-})
+  ]
+}
