64c2b7addda4e715860110d615ad73ea35cb6345
Kimchi-alive <kto@alivekirken.dk>
2021-06-02 11:23:25 +0200

    update package

--- gatsby-config.esm.js
+++ gatsby-config.esm.js
@@ -7,8 +7,8 @@ export default (props) => ({
     {
       resolve: 'gatsby-source-prismic',
       options:{
-        repositoryName:"gatsby-theme",
-        accessToken:"MC5ZTFRtZHhBQUFDWUFlcExw.77-9ce-_ve-_ve-_vSHvv73vv70U77-977-9PAXvv73vv73vv73vv71LNO-_ve-_vW9k77-977-977-977-977-977-977-977-977-9",
+        repositoryName:props.prismicRepo,
+        accessToken:props.prismicToken,
         linkResolver: ({ node, key, value }) => (doc) => {
           // do something with links here
         },
