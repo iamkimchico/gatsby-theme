cd7e662fe8463737939eac08ddcb29ee8f178df3
Kimchi-alive <kto@alivekirken.dk>
2021-06-02 11:36:56 +0200

    update package

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -1,3 +1,11 @@
-export const createPages = () => {
-  console.log("creating page")
+import * as queries from "./prismic/queries"
+
+export const createPages = ({graphql, actions}) => {
+  const { createPage } = actions
+
+
+  const fetchPages = await graphql(queries.page);
+  const pages = fetchPages.data.allPrismicPages.edges;
+
+  console.log(pages);
 }
